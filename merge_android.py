f3975650092a8b65ca3fdd089cbc63c7c397c3dd
Chinuno Usami <usami@chinuno.com>
2018-08-26 15:23:43 +0800

    Fix bangumi exception.

--- merge_android.py
+++ merge_android.py
@@ -11,17 +11,17 @@ def get_name(path):
         info = f.read()
         parsed = json.loads(info)
         title = parsed['title']
-        page_data = parsed['page_data']
-        if page_data:
-            return title+str(page_data['page'])+page_data['part']
-        else:
+        if 'page_data' in parsed:
+            page_data = parsed['page_data']
+            return title + str(page_data['page']) + page_data['part']
+        elif 'ep' in parsed:
             ep = parsed['ep']
-            return title+str(ep['index'])+ep['index_title']
+            return title + str(ep['index']) + ep['index_title']
 
 
 def do_merge(dirname, title):
-    title = title.replace("/","")
-    os.system(os.path.dirname(os.path.abspath(__file__)) + '/merge.sh "'+ dirname + '" "' + title + '"')
+    title = title.replace("/", "")
+    os.system(os.path.dirname(os.path.abspath(__file__)) + '/merge_android.sh "'+ dirname + '" "' + title + '"')
 
 
 def trav_dirs(root):
